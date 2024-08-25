e5563662cc3e7143626aea98e509f419a5fea4f6
hosenur <hosenur.dev@gmail.com>
2024-08-26 02:46:07 +0530

    remove build command from auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@repo/ui/components/ui/button"
 
 export default async function Home() {
   return (
-    <div>
-      <Button>Hello</Button>
+    <div className="max-w-4xl mx-auto py-6">
+      <h1 className="text-4xl font-medium">flipper</h1>
     </div>
   )
 }
