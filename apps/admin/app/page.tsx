cb27dc7b919d55f4502abc99156e374a39a323a1
hosenur <hosenur.dev@gmail.com>
2024-08-26 03:12:56 +0530

    fixed

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { prisma } from '../lib/database'
 
 export default async function Home() {
-  const kv = await prisma.kV.findFirst()
   return (
     <div>
-      {JSON.stringify(kv)}
+      hi
     </div>
   )
 }
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ["@repo/ui"],
+    transpilePackages: [],
 };
 
 export default nextConfig;
