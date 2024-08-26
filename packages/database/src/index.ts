aca0082d325eb2cbed76049dc3aa3c8316eda8a6
hosenur <hosenur.dev@gmail.com>
2024-08-26 12:03:44 +0530

    test

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,11 +1,3 @@
-import 'dotenv/config';
 import { PrismaClient } from "@prisma/client";
-console.log(process.env)
-export const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL
-        }
-    }
-});
+export const prisma = new PrismaClient();
 
